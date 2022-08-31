1db03c11290ef2053b30303869d49c288fef6350
Nicolas Suarez <106325313+NicolasABSuarezB@users.noreply.github.com>
2022-08-31 10:57:17 -0300

    Se dinamiza la vista del carrito

--- site/controllers/productsControllers.js
+++ site/controllers/productsControllers.js
@@ -1,9 +1,13 @@
+let productos = require('../data/productos.json')
 module.exports={
     detalle: (req,res)=>{
-        return res.render('detalle')
+        return res.render('detalle',{
+            productos
+        });
     },
     carrito: (req,res)=>{
-        return res.render('carrito')
+        return res.render('carrito',{
+            productos
+        });
     }
-
 }
